6686aae2fa625379401d13d5c75a5e844b413707
Tim Jaanson <jaanson.tim@gmail.com>
2019-04-17 12:11:52 +0300

    main scraping

--- back-end/models/schedulemodel.js
+++ back-end/models/schedulemodel.js
@@ -1,30 +1,34 @@
 const request = require('request')
 const cheerio = require('cheerio')
 const iconv = require('iconv-lite')
+const https = require('https')
 
-const url = 'http://www.tlu.ee/masio/?id=ryhm&ryhm=IFIFB-2#mASIO'
+const url = 'https://www.tlu.ee/masio/?id=ryhm&ryhm=IFIFB-2#mASIO'
 
 const getSchedule = url => {
-    request(url, (error, res, html) => {
-        if(!error && res.statusCode == 200) {
-            const $ = cheerio.load(iconv.decode(html, 'ISO-8859-16'))
-            //const days = $('#mASIO .dayname')
+    https.get(url, function(res) {
+        var chunks = []
+        res.on('data', function(chunk) {
+            chunks.push(chunk)
+        })
+        res.on('end', function() {
+            var decodedBody = iconv.decode(Buffer.concat(chunks), 'win1252')
+            const $ = cheerio.load(decodedBody)
             const days = $('.dp div')
             days.each((i, element) => {
                 if ($(element).hasClass("dayname")) {
-                    console.log($(element).text())
+                    const day = $(element).text()
+                    console.log(day)
+                } else if($(element).hasClass("lline")) {
+                    const classTime = $(element).children().first()
+                    const className = $(classTime).next().text()
+                    const classProf = $(element).children('.small').text()
+                    const classRoom = $(element).children().remove().end().text().replace(/\s\s+/g, '')
+                    console.log(classRoom+ ' | ' +classTime.text()+ ' | ' +className+ ' | ' +classProf)
+                    //console.log($(element).text().replace(/\s\s+/g, ''))
                 }
-                
-                /*
-                $(element).each((lessonIterator, lessonEl) => {
-                    const lesson = $(lessonEl).text()
-                    //console.log(lesson)
-                })
-                const day = $(element).next().text()
-                //console.log(i, day)
-                */
             })
-        }
+        })
     })
 }
 
